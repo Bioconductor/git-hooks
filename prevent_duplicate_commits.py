ad5b79d1147e1095ef0482e20b04475d2b298de1
Nitesh Turaga <nitesh.turaga@gmail.com>
2017-09-01 10:05:41 -0400

    Re-enable pre-receive hook for duplicate commits
    This should check on duplicated SVN commits`

--- prevent_duplicate_commits.py
+++ prevent_duplicate_commits.py
@@ -2,9 +2,12 @@
 
 import subprocess
 import sys
+import re
 
 # Global variables used by pre-recieve hook
 
+GIT_COMMIT_LIST_LENGTH = "10"
+SVN_COMMIT_REGEX = re.compile(".*git-svn-id: .*@([0-9]{6})")
 ZERO_COMMIT = "0000000000000000000000000000000000000000"
 ERROR_DUPLICATE_COMMITS = """Error: duplicate commits.
 
@@ -28,47 +31,38 @@ Use
 to see body of commits.
 """
 
+def get_svn_revision(commit):
+    body = subprocess.check_output([ "git", "show", "--format=%b", commit ])
+    revision = SVN_COMMIT_REGEX.match(body)
+    if revision != None:
+        revision = revision.group(1)
+    return revision
+
+
 def prevent_duplicate_commits(oldrev, newrev, refname):
-    """Pre-receive hook to check for duplicate commits."""
+    """Pre-receive hook to check for duplicate SVN commits."""
     try:
-        commit_list = subprocess.check_output(["git",
-                                               "rev-list",
-                                               newrev, "-n", "20"])
+        commit_list = subprocess.check_output([
+            "git", "rev-list", newrev, "-n", GIT_COMMIT_LIST_LENGTH
+        ])
     except Exception as e:
         print("Exception: %s" % e)
         pass
     commit_list = commit_list.split("\n")
     commit_list = [item for item in commit_list if len(item)>0]
-    # For each of the first 10 pairs, check diff
 
+    # For each of the first GIT_COMMIT_LIST_LENGTH pairs, check diff
     for i in xrange(len(commit_list) - 1):
         first = commit_list[i]
         second = commit_list[i+1]
 
-        # use 'show' to test for empty commits,
-        # else git diff will report no diffs
-        body1 = subprocess.check_output(["git", "show",
-                                         "--format=%b", first]).strip()
-        if body1.startswith('git-svn-id'):
-            body1 = body1.splitlines()[1:]
-        ## If body1 is empty
-        if not body1:
-            continue
-
-        body2 = subprocess.check_output(["git", "show",
-                                         "--format=%b", second]).strip()
-
-        if body2.startswith('git-svn-id'):
-            body2 = body2.splitlines()[1:]
-        ## if body2 is empty
-        if not body2:
-            continue
-
-        # Get diff of two commits
-        diff = subprocess.check_output(["git", "diff", first, second])
-        # If the diff of two commits is empty, means they are the same.
-        # i.e duplicate
-        if not diff:
-            print(ERROR_DUPLICATE_COMMITS % (first, second))
-            sys.exit(1)
+        rev1 = get_svn_revision(first)
+        rev2 = get_svn_revision(second)
+        if rev1 and (rev1 == rev2):
+            diff = subprocess.check_output(["git", "diff", first, second])
+            # If the diff of two commits is empty, means they are the same.
+            # i.e duplicate
+            if not diff:
+                print(ERROR_DUPLICATE_COMMITS % (first, second))
+                sys.exit(1)
     return
