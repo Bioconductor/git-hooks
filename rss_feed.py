d9d0e3166ff9cb656ea99d262ef249dc7c14e276
Nitesh Turaga <nitesh.turaga@gmail.com>
2017-12-01 11:01:50 -0500

    explicit split at newline

--- rss_feed.py
+++ rss_feed.py
@@ -63,7 +63,7 @@ def rss_feed(oldrev, newrev, refname, fpath, length):
         pass
     if latest_commit:
         ## If more than one commit to unpack
-        latest_commit = latest_commit.split()
+        latest_commit = latest_commit.split("\n")
         print("latest_commit: ", latest_commit)
         for commit in latest_commit:
             print("commit: ", commit)
