a2ce18f542c2b085e5924f16effab1c9cbad17ab
Nitesh Turaga <nitesh.turaga@gmail.com>
2017-12-14 06:48:56 -0500

    Add locks and git commands

--- rss_feed.py
+++ rss_feed.py
@@ -1,13 +1,13 @@
 import subprocess
 import datetime
-import re
+# import re
 from os.path import basename, abspath
-from xml.etree.ElementTree import parse
+from xml.etree.ElementTree import fromstring
 import logging
-logging.basicConfig(filename='post-recieve.log',level=logging.DEBUG)
 
 
-ENTRY="""    <item>
+ENTRY="""
+    <item>
       <title>%s</title>
       <link>https://www.bioconductor.org</link>
       <description>%s</description>
@@ -18,39 +18,9 @@ ENTRY="""    <item>
 """
 
 
-def limit_feed_length(fh, length):
-    """ This is run everytime the feed reaches limit"""
-    doc = parse(fh)
-    root = doc.getroot()
-    # Get all RSS item
-    channel_root = root.find("channel")
-    items = channel_root.findall("item")
-    # check length
-    if len(items) > length:
-        extra_items = items[length:]
-        for item in extra_items:
-            channel_root.remove(item)
-    fh.seek(0)
-    fh.truncate()
-    doc.write(fh)
-    fh.write("\n")
-    return
-
-
-def write_feed(entry, fh):
-    """Write feed to the beginning of the file"""
-    text = fh.read()
-    text = re.sub(r'<copyright />\n',
-                  '<copyright />\n' + entry,
-                  text)
-    fh.seek(0)
-    fh.write(text)
-    fh.flush()
-    return
-
-
-def rss_feed(oldrev, newrev, refname, fh, length):
+def rss_feed(oldrev, newrev, refname, length):
     """Post receive hook to check start Git RSS feed"""
+    entry_list = []
     try:
         latest_commit = subprocess.check_output([
             "git", "log", oldrev + ".." + newrev,
@@ -68,7 +38,6 @@ def rss_feed(oldrev, newrev, refname, fh, length):
         latest_commit = latest_commit.split("\n")
         # Reverse if there are multiple commits
         for commit in latest_commit[::-1]:
-            logging.info("commit: ", commit)
             commit_id, author, commit_msg, timestamp = commit.split("|")
             pubDate = datetime.datetime.fromtimestamp(
                         float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
@@ -78,13 +47,6 @@ def rss_feed(oldrev, newrev, refname, fh, length):
                              author,
                              pubDate,
                              commit_id)
-            # Write FEED and sleep to avoid race condition
-            try:
-                write_feed(entry, fh)
-            except IOError as e:
-                logging.error("Error writing feed", e)
-            try:
-                limit_feed_length(fh, length)
-            except Exception as e:
-                logging.error("Error limiting feed size", e)
-    return
+            # Add entry as element in xml.etree
+            entry_list.append(fromstring(entry))
+    return entry_list
